a56bf14842871d30f2a8ae5c4727b3e0dd20ea8e
jarwonozt <jarwonoztaveiro@gmail.com>
2022-06-24 13:06:45 +0700

    META og and twitter card

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,17 +5,51 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Butschster\Head\Facades\Meta;
-// use Butschster\Head\MetaTags\MetaInterface;
+use Butschster\Head\Packages\Entities\OpenGraphPackage;
 
 class HomeController extends Controller
 {
-    public function index(){
-        Meta::prependTitle('Home page')
+    public function __construct()
+    {
+        Meta::prependTitle('Home')
+                ->setContentType('text/html')
                 ->setViewport('width=device-width, initial-scale=1')
                 ->setDescription('Jarwonotech adalah layanan web khusus untuk berbagi tutorial trips dan trik seputar laravel')
                 ->setKeywords(['jarwonozt', 'jarwonoztech', 'laravel', 'blog'])
                 ->setRobots('nofollow,noindex')
-                ->setContentType('text/html');
+                ->addMeta('author', [
+                    'content' => 'jarwonozt',
+                ]);
+
+        $og = new \Butschster\Head\Packages\Entities\OpenGraphPackage('og_meta');
+
+        $og->setType('website')
+            ->setSiteName('Jarwonoztech')
+            ->setTitle('Home Page')
+            ->setDescription('Jarwonotech adalah layanan web khusus untuk berbagi tutorial trips dan trik seputar laravel')
+            ->setUrl(config('app.url'))
+            ->setLocale('id_ID')
+            ->addImage(asset('frontend').'/assets/images/logo-2.png');
+
+        $og->toHtml();
+        Meta::registerPackage($og);
+
+        $card = new \Butschster\Head\Packages\Entities\TwitterCardPackage('twitter_meta');
+
+        $card->setType('summary')
+        ->setSite('@jarwonoztech')
+        ->setTitle('Jarwonoztech')
+        ->setDescription('Jarwonotech adalah layanan web khusus untuk berbagi tutorial trips dan trik seputar laravel')
+        ->setCreator('@jarwonoztech')
+        ->setImage(asset('frontend').'/assets/images/logo-2.png')
+        ->addMeta('image:alt', 'Picture of Jarwonoztech');
+
+        $card->toHtml();
+        Meta::registerPackage($card);
+    }
+
+    public function index(){
+
 
         // dd(PagesController::sliderArticles(4));
         return view('frontend.home', [
