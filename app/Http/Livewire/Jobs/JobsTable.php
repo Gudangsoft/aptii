b83476c73c156f428c818a59b65c6bf9579fcdff
jarwonozt <jarwonoztaveiro@gmail.com>
2022-06-29 18:38:58 +0700

    model jobs migration

---
+++ app/Http/Livewire/Jobs/JobsTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Jobs;
+
+use Livewire\Component;
+
+class JobsTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.jobs.jobs-table');
+    }
+}
