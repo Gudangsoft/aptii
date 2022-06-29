388f37d23fea2cc3fb9f3a51f1aa165e81f42133
jarwonozt <jarwonoztaveiro@gmail.com>
2022-06-29 21:37:32 +0700

    check all data jobs table livewire

--- app/Http/Livewire/Jobs/JobsTable.php
+++ app/Http/Livewire/Jobs/JobsTable.php
@@ -11,10 +11,53 @@ class JobsTable extends Component
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
+    public $selectJobs = [];
+    public $selectAll = false;
+    public $bulkDisabled = true;
+    public $statusSelected = false;
+
     public function render()
     {
+        $this->bulkDisabled = count($this->selectJobs) < 1;
         return view('livewire.jobs.jobs-table', [
             'data' => Jobs::orderByDesc('created_at')->paginate(10),
         ]);
     }
+
+    public function deleteSelected(){
+        Jobs::query()
+            ->whereIn('id', $this->selectJobs)
+            ->delete();
+        $this->selectJobs = [];
+        $this->selectAll = false;
+    }
+
+    public function selectAll($value){
+        if($value){
+            $this->selectJobs = Jobs::pluck('id');
+            $this->statusSelected = true;
+        }else{
+            $this->selectJobs = [];
+        }
+    }
+
+    public function unselectedJobs(){
+        $this->selectJobs = [];
+        $this->selectAll = false;
+        $this->statusSelected = false;
+
+    }
+
+    public function updateStatus($value){
+        Jobs::query()
+            ->whereIn('id', $this->selectJobs)
+            ->update([
+                'status' => $value
+            ]);
+
+        $this->selectJobs = [];
+        $this->selectAll = false;
+        $this->statusSelected = false;
+    }
+
 }
--- resources/views/livewire/jobs/jobs-table.blade.php
+++ resources/views/livewire/jobs/jobs-table.blade.php
@@ -1,55 +1,93 @@
 <div>
+    <div class="content-header row">
+        <div class="content-header-left col-md-9 col-12 mb-2">
+            <div class="row breadcrumbs-top">
+                <div class="col-7">
+                    <h2 class="content-header-title float-left mb-0">Jobs List</h2>
+                    <div class="breadcrumb-wrapper">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="breadcrumb-item active">Jobs
+                            </li>
+                            <li class="breadcrumb-item active">Jobs List
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="col-5 d-flex-">
 
-    <div class="row" id="basic-table">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Table Basic</h4>
                 </div>
-                <div class="card-body">
-                    <p class="card-text">
-                        Using the most basic table Leanne Grahamup, here’s how <code>.table</code>-based tables look in Bootstrap. You
-                        can use any example of below table for your table and it can be use with any type of bootstrap tables.
-                    </p>
-                </div>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Type</th>
-                                <th>Specialis</th>
-                                <th>Location</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($data as $row)
+            </div>
+        </div>
+    </div>
+    <div class="content-body">
+        <div class="row" id="basic-table">
+            <div class="col-12">
+                <div class="card">
+                    @if (count($selectJobs) > 0)
+                    <div class="card-body">
+                        <p class="card-text">
+                            {{ count($selectJobs) }} data selected, <button wire:click="unselectedJobs()" class="btn btn-sm btn-primary">Cancel</button>
+                            <div class="btn-group">
+                                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton6" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Actions
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton6">
+                                    <a class="dropdown-item" href="javascript:void(0);" wire:click="updateStatus(0)">Hidden</a>
+                                    <a class="dropdown-item" href="javascript:void(0);" wire:click="updateStatus(1)">Show</a>
+                                    <a class="dropdown-item" href="javascript:void(0);" wire:click="deleteSelected()">Delete</a>
+                                </div>
+                            </div>
+                        </p>
+                    </div>
+                    @endif
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
                                 <tr>
-                                    <td>
-                                        <span class="font-weight-bold">{{ $row->title }}</span>
-                                    </td>
-                                    <td>{{ $row->type }}</td>
-                                    <td>
-                                        {{ $row->specialisation }}
-                                    </td>
-                                    <td><span class="badge badge-light-primary mr-1">{{ $row->work_location }}</span></td>
-                                    <td>
-                                        <a href="" class="btn btn-sm btn-primary" title="View"><i data-feather="eye"></i></a>
-                                        <a href="" class="btn btn-sm btn-success" title="Edit"><i data-feather="edit"></i></a>
-                                        <a href="" class="btn btn-sm btn-danger" title="Delete"><i data-feather="trash"></i></a>
-                                    </td>
+                                    <th><input type="checkbox" wire:click="selectAll(1)" wire:model="selectAll"></th>
+                                    <th>Title</th>
+                                    <th>Type</th>
+                                    <th>Specialis</th>
+                                    <th>Location</th>
+                                    <th>Actions</th>
                                 </tr>
-                            @empty
-                            <tr>
-                                <td colspan="5" class="pt-2 pb-2">Data not found !</td>
-                            </tr>
-                            @endforelse
+                            </thead>
+                            <tbody>
+                                @forelse ($data as $row)
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" value="{{ $row->id }}" wire:model="selectJobs" id="a">
+                                        </td>
+                                        <td>
+                                            @if ($row->status < 1)
+                                                <i class="text-danger" data-feather="x-octagon"></i>
+                                            @endif
+                                            <span class="font-weight-bold">{{ $row->title }}</span>
+                                        </td>
+                                        <td>{{ $row->type }}</td>
+                                        <td>
+                                            {{ $row->specialisation }}
+                                        </td>
+                                        <td><span class="badge badge-light-primary mr-1">{{ $row->work_location }}</span></td>
+                                        <td>
+                                            <a href="" class="btn btn-sm btn-primary" title="View"> View</a>
+                                            <a href="" class="btn btn-sm btn-success" title="Edit"> Edit</a>
+                                            <a href="" class="btn btn-sm btn-danger" title="Delete"> Delete</a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                <tr>
+                                    <td colspan="5" class="pt-2 pb-2">Data not found !</td>
+                                </tr>
+                                @endforelse
 
-                        </tbody>
-                    </table>
-                    <div class="d-flex justify-content-center mt-2">
-                        {{ $data->links() }}
+                            </tbody>
+                        </table>
+                        <div class="d-flex justify-content-center mt-2">
+                            {{ $data->links() }}
+                        </div>
                     </div>
                 </div>
             </div>
