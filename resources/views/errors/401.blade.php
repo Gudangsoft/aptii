e1520ac91a85e8e193087896a75168ef794b1d72
jarwonozt <jarwonoztaveiro@gmail.com>
2022-09-01 18:13:31 +0700

    dashboad encode

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,5 +1,33 @@
-@extends('errors::minimal')
 
-@section('title', __('Unauthorized'))
-@section('code', '401')
-@section('message', __('Unauthorized'))
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Traktir Kopi Developer</title>
+    <link type="text/css" rel="stylesheet" href="https://tsparticles.github.io/404-templates/space/css/404.css" />
+</head>
+<style>
+    .msg{
+        text-align: center;
+        color: #fff;
+        font-family: "Tahoma", sans-serif;
+        font-size: 30px;
+        margin-bottom: 0px;
+        font-weight: 800;
+    }
+</style>
+
+<body class="permission_denied">
+    <div id="tsparticles"></div>
+    <div class="denied__wrapper">
+        <h1>401</h1>
+        <h3>LOST IN <span>SPACE</span></h3>
+        <div class="msg">Untuk mendapatkan akses aplikasi silakan hubungi 082145095183</div>
+        <a href="https://wa.me/082145095183"><button class="denied__link">WHATSAPP</button></a>
+    </div>
+
+    <script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/tsparticles@1.18.11/dist/tsparticles.min.js"></script>
+    <script type="text/javascript" src="js/404.js"></script>
+
+</html>
