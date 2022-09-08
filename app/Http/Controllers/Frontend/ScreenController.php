51bc26d5074e777413966a90f2a440fa8878ab0e
jarwonozt <jarwonoztaveiro@gmail.com>
2022-09-08 12:44:30 +0700

    fix meta detail post

--- app/Http/Controllers/Frontend/ScreenController.php
+++ app/Http/Controllers/Frontend/ScreenController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\MetaController;
 use App\Models\Admin\Configuration;
 use App\Models\Prosiding\CustomerCare;
+use App\Models\Prosiding\Event;
 use Illuminate\Http\Request;
 use Butschster\Head\Facades\Meta;
 use Illuminate\Support\Arr;
@@ -15,7 +17,7 @@ use RobertSeghedi\News\Models\News;
 class ScreenController extends Controller
 {
     public function post($slug){
-        self::meta($slug);
+        MetaController::meta($slug);
         News::counter($slug);
         return view('frontend.articles.detail', [
             'data'          => PageController::article($slug),
@@ -27,7 +29,7 @@ class ScreenController extends Controller
     }
 
     public function seminar($slug){
-        HomeController::meta('Seminar');
+        MetaController::metaSeminar($slug);
         return view('frontend.events.detail', [
             'data'          => PageController::seminar($slug),
             'popular'       => PagesController::popularArticle(),
@@ -37,47 +39,7 @@ class ScreenController extends Controller
         ]);
     }
 
-    public static function meta($slug){
 
-        $web    = Configuration::where('status', 1)->first();
-        $data   = PageController::article($slug);
-
-        Meta::prependTitle($data->title)
-                ->setContentType('text/html')
-                ->setViewport('width=device-width, initial-scale=1')
-                ->setDescription(Str::words(html_entity_decode($data->content), 15))
-                ->setKeywords(explode(',', $data->tags))
-                ->setRobots('nofollow,noindex')
-                ->addMeta('author', [
-                    'content' => $data->getUser->name,
-                ]);
-
-        $og = new \Butschster\Head\Packages\Entities\OpenGraphPackage('og_meta');
-
-        $og->setType('website')
-            ->setSiteName(config('app.name'))
-            ->setTitle($data->title)
-            ->setDescription(Str::words(html_entity_decode($data->content), 15))
-            ->setUrl(config('app.url').'/post/'.$data->slug)
-            ->setLocale('id_ID')
-            ->addImage(asset('frontend').'/articles/thumbnail/'.$data->image);
-
-        $og->toHtml();
-        Meta::registerPackage($og);
-
-        $card = new \Butschster\Head\Packages\Entities\TwitterCardPackage('twitter_meta');
-
-        $card->setType('summary')
-        ->setSite('@prosiding_app')
-        ->setTitle($data->title)
-        ->setDescription(Str::words(html_entity_decode($data->content), 15))
-        ->setCreator('@prosiding_app')
-        ->setImage(asset('frontend').'/articles/thumbnail/'.$data->image)
-        ->addMeta('image:alt', $data->title);
-
-        $card->toHtml();
-        Meta::registerPackage($card);
-    }
 
     public function contact(){
         HomeController::meta('Kontak Kami');
@@ -91,4 +53,6 @@ class ScreenController extends Controller
             'configuration' => Configuration::orderBy('created_at')->first()
         ]);
     }
+
+
 }
