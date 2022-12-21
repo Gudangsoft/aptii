9bb1ec55d428306511a11e2224d2badc771987bb
jarwonozt <jarwonoztaveiro@gmail.com>
2022-12-21 17:09:52 +0700

    fix sidebar

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -3,13 +3,17 @@
     @include('client.layouts.banner')
     {{-- @include('client.sections.service') --}}
     @include('client.sections.posts', ['data' => $posts])
-    <div class="row">
-        <div class="col-lg-6 col-md-6">
-            @include('client.sections.agenda', ['title' => 'Agenda', 'data' => $agenda])
+    <section class="section-padding pt-0 mt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    @include('client.sections.agenda', ['title' => 'Agenda', 'data' => $agenda])
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    @include('client.sections.kegiatan', ['title' => 'Kegiatan', 'data' => $activities])
+                </div>
+            </div>
         </div>
-        <div class="col-lg-6 col-md-6">
-            @include('client.sections.kegiatan', ['title' => 'Kegiatan', 'data' => $activities])
-        </div>
-    </div>
+    </section>
 
 </x-frontend-master>
