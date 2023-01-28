4687eeff416765c8a10db8d892fec142b7cf58f0
jarwonozt <jarwonoztaveiro@gmail.com>
2023-01-28 19:29:23 +0700

    update header

--- resources/views/client/layouts/header.blade.php
+++ resources/views/client/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<header class="header-style-1">
+<header class="header-style-1"> 
     <div class="header-navbar navbar-sticky">
         <div class="container">
             <div class="d-flex align-items-center justify-content-between">
@@ -12,16 +12,34 @@
                 </div>
 
                 <div class="offcanvas-icon d-block d-lg-none">
-                    <a href="#" class="nav-toggler"><i class="fal fa-bars"></i></a>
+                    <a href="#" class="nav-toggler"><i class="fal fa-bars"></i></a> 
                 </div>
 
-                <div class="header-category-menu d-none d-xl-block">
+                <div class="header-category-menu d-none">
+                    <ul>
+                        <li class="has-submenu">
+                            <a href="#"><i class="fa fa-th me-2"></i>Categories</a>
+                            <ul class="submenu">
+                                <li>
+                                    <a href="#">Design</a>
+                                    <ul class="submenu">
+                                        <li><a href="#">Design Tools</a></li>
+                                        <li><a href="#">Photoshop mastering</a></li>
+                                        <li><a href="#">Adobe Xd Deisgn</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Developemnt</a></li>
+                                <li><a href="#">Marketing</a></li>
+                                <li><a href="#">Freelancing</a></li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
 
                 <nav class="site-navbar ms-auto">
-                        @php
-                            $menu = \App\Models\Menu::where('status', 1)->orderBy('order', 'ASC')->get();
-                        @endphp
+                    @php
+                        $menu = \App\Models\Menu::where('status', 1)->orderBy('order', 'ASC')->get();
+                    @endphp
                     <ul class="primary-menu">
                         @foreach ($menu as $item)
                             @if ($item->parent_id == 0 && $item->category_id == 1)
@@ -41,23 +59,11 @@
                                 @endif
                             @endif
                         @endforeach
-                        @if (Auth::check())
-                            <li class="d-sm-block d-md-block d-lg-none d-xl-none">
-                                <a href="{{ route('dashboard') }}" class="login"><i class="fa fa-cog"></i> Dashboard</a>
-                            </li>
-                        @else
-                            <li class="d-sm-block d-md-block d-lg-none d-xl-none">
-                                <a href="{{ route('login') }}" class="login">Login</a>
-                            </li>
-                            <li class="d-sm-block d-md-block d-lg-none d-xl-none">
-                                <a href="{{ route('register') }}" class="login">Register</a>
-                            </li>
-                        @endif
+                        
                     </ul>
 
                     <a href="#" class="nav-close"><i class="fal fa-times"></i></a>
                 </nav>
-
                 <div class="header-btn d-none d-xl-block">
                     @if (Auth::check())
                         {{-- <form method="POST" action="{{ route('logout') }}">
